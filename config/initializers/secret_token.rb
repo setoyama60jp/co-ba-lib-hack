051952508af61421f5727aa8712251d0595df355
Masato setoyama <setoyama60jp@gmail.com>
2012-07-20 19:37:28 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoBaLibHack::Application.config.secret_token = 'f3b0cf454d958591afc0fa429e7a2d337e1ac26262f980f86e54f13a6f78014cc111d5b63ead19fa754f076d5fcc0acf46826fe124397accd508a2b2c729781a'
