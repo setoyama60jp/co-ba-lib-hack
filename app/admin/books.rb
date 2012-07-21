537d2077ec615b232336ecd5901fc6d11b38ca1e
Masato setoyama <setoyama60jp@gmail.com>
2012-07-21 21:12:13 +0900

    active_adminの設定を修正。

--- app/admin/books.rb
+++ app/admin/books.rb
@@ -1,20 +1,3 @@
 ActiveAdmin.register Book do
 
-
-  #編集画面に表示したい項目をここに並べる
-  form do |f|
-    f.inputs "Details" do
-      f.input :booklog_id
-      f.input :asin
-      f.input :booklog_url
-      f.input :title
-      f.input :author
-      f.input :image_url
-      f.input :image_width
-      f.input :image_height
-      f.input :category
-      f.input :book_shelf
-    end
-    f.buttons
-  end
 end
