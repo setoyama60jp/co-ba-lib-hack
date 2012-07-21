7a89ae9ada2e2eee265b7273cc8da4f056daa9c3
Masato setoyama <setoyama60jp@gmail.com>
2012-07-22 00:56:39 +0900

    ActiveAdminのbook_shelfのindex画面を使いやすく修正。

--- app/admin/book_shelves.rb
+++ app/admin/book_shelves.rb
@@ -1,3 +1,16 @@
 ActiveAdmin.register BookShelf do
-  
+  index do
+      column :id do |book_shelf|
+        link_to book_shelf.id.to_s, resource_path(book_shelf)
+      end
+      column :address
+      column :photo_url
+      column :user do |book_shelf|
+        book_shelf.try(:coba_user).try(:name)
+      end
+      column :introduction
+      column :book_shelf_position
+
+      default_actions
+    end
 end
