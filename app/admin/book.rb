cc386c4852f7a50427f3da851b49714b03a9e100
Masato setoyama <setoyama60jp@gmail.com>
2012-07-21 20:50:41 +0900

    Bookの入力画面を作成。

--- app/admin/book.rb
+++ app/admin/book.rb
@@ -4,7 +4,16 @@ ActiveAdmin.register Book do
   #編集画面に表示したい項目をここに並べる
   form do |f|
     f.inputs "Details" do
+      f.input :booklog_id
+      f.input :asin
+      f.input :booklog_url
       f.input :title
+      f.input :author
+      f.input :image_url
+      f.input :image_width
+      f.input :image_height
+      f.input :category
+      f.input :book_shelf_id
     end
     f.buttons
   end
